3c1b5c9e7035b2242217b23ecf9b299813ce51fb
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-08-11 15:51:51 +0530

    Add files via upload

---
+++ MATRICE SUB.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int x,y,a[100][100],b[100][100],c[100][100],i,j;
+
+    printf("Enter the number of rows (Max 100): ");
+    scanf("%d", &x);
+
+    printf("Enter the number of columns (Max 100): ");
+    scanf("%d", &y);
+
+    printf("Enter the first matrix :\n");
+
+    for(i=0;i<x;i++)
+{
+        for(j=0;j<y;j++)
+{
+        scanf("\t%d",&a[i][j]);
+}
+    printf("\n");
+}
+    printf("Recheck your matrix\n");
+
+    for(i=0;i<x;i++)
+{
+        for(j=0;j<y;j++)
+{
+            printf("\t%d",a[i][j]);
+
+}
+printf("\n");
+}
+
+    printf("\nEnter the second matrix:\n");
+
+    for(i=0;i<x;i++)
+{
+        for(j=0;j<y;j++)
+{
+            scanf("\t%d",&b[i][j]);
+}
+    printf("\n");
+}
+   printf("Recheck your matrix\n");
+
+    for(i=0;i<x;i++)
+{
+        for(j=0;j<y;j++)
+{
+            printf("\t%d",b[i][j]);
+}
+printf("\n");
+}
+    for(i=0;i<x;i++)
+{
+        for(j=0;j<y;j++)
+{
+            c[i][j]=a[i][j]-b[i][j];
+}
+}
+    printf("\n\nDifference of two matrix :\n");
+    for(i=0;i<x;i++)
+{
+        for(j=0;j<y;j++)
+{
+            printf("\t%d",c[i][j]);
+}
+printf("\n");
+}
+getch();
+}
