61ad56ccae2c1f34b0c9354575356add911c491e
TECH-HACKER16 <142000714+TECH-HACKER16@users.noreply.github.com>
2023-08-11 14:17:38 +0530

    Add files via upload

---
+++ SWAPPING USING THIRD VARIABLE.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    // SWAPING TWO NUMBERS USING THIRD VARIABLE
+
+       int A = 10,B = 20,C;
+
+       C = A;
+
+       A = B;
+
+       B = C;
+
+       printf("A = %d \nB = %d",A,B);
+
+       getch();
+
+       return(0);
+
+
+}
